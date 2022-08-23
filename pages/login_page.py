f39e8c14f3966f09f7ab6d163a1cff28513c91eb
Onishchenko Serhii <serhiionischenko@gmail.com>
2022-08-23 12:45:28 +0200

    Task3_1
    add new methods in to the login page and add TestMedimPages

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,3 +8,9 @@ class LoginPage(BasePage):
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
+
+    def type_in_password(self, password):
+        self.field_send_keys(self.password_field_xpath, password)
+
+    def click_on_the_element_button(self):
+        self.click_on_the_element(self.sign_in_button_xpath)
